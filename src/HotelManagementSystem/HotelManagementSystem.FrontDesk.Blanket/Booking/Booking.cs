0f023962593cdac1bbde19f2046e44f85516db98
AkshayN01 <nambly77@gmail.com>
2024-04-10 12:45:25 +0100

    Added a blanket layer in Front Desk Api

---
+++ src/HotelManagementSystem/HotelManagementSystem.FrontDesk.Blanket/Booking/Booking.cs
@@ -0,0 +1,20 @@
+﻿using HotelManagementSystem.FrontDesk.DataAccess;
+using HotelManagementSystem.Library.Services;
+using HotelManagementSystem.Library.Services.Data.FrontDesk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem.FrontDesk.Blanket.Booking
+{
+    public class Booking
+    {
+        private readonly BookingService _service;
+        public Booking(BookingService bookingService) 
+        {
+            _service = bookingService;
+        }
+    }
+}
---
+++ src/HotelManagementSystem/HotelManagementSystem.Library/Services/RoomService.cs
@@ -0,0 +1,13 @@
+﻿using HotelManagementSystem.Library.Services.Data.FrontDesk;
+
+namespace HotelManagementSystem.Library.Services
+{
+    public class RoomService
+    {
+        public readonly IFrontDeskUnitOfWork _frontDeskUnitOfWork;
+        public RoomService(IFrontDeskUnitOfWork frontDeskUnitOfWork)
+        {
+            _frontDeskUnitOfWork = frontDeskUnitOfWork;
+        }
+    }
+}
