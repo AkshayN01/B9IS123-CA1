191d8e61770b55a387f82a34489edcaed1f8997d
AkshayN01 <nambly77@gmail.com>
2024-04-16 19:43:50 +0100

    Added docker support to the solution

--- src/HotelManagementSystem/HotelManagementSystem.IdentityServer/Program.cs
+++ src/HotelManagementSystem/HotelManagementSystem.IdentityServer/Program.cs
@@ -45,11 +45,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.Use(async (ctx, next) =>
-{
-    ctx.SetIdentityServerOrigin("https://localhost:7016");
-    await next();
-});
+//app.Use(async (ctx, next) =>
+//{
+//    ctx.SetIdentityServerOrigin("https://localhost:7016");
+//    await next();
+//});
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
