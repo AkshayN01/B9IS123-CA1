cd007758abc7869ec7ba4bd4fc26b38a1bf6911c
Jitty Thomas <jittythomas774@gmail.com>
2024-04-17 21:37:28 +0100

    submit

--- src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/booking-register/booking-register.component.ts
+++ src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/booking-register/booking-register.component.ts
@@ -18,5 +18,16 @@ export class BookingRegisterComponent {
         checkOutDate: ['', Validators.required]
       });
     }
+    onSubmit() {
+        if (this.bookingForm.valid) {
+          // Process the booking data, e.g., send it to a backend server
+          console.log(this.bookingForm.value);
+          // Here you can make an HTTP request to your backend to save the booking
+        } else {
+          // Handle form validation errors
+          alert('Please fill out all required fields.');
+        }
+      }
+    }
   
     
