ae55792cb39e7f1e3aa01c6c08cb97c3f318e6f6
Jitty Thomas <jittythomas774@gmail.com>
2024-04-17 21:37:08 +0100

    component

--- src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/booking-register/booking-register.component.ts
+++ src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/booking-register/booking-register.component.ts
@@ -6,3 +6,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './booking-register.component.html',
   styleUrls: ['./booking-register.component.css']
 })
+export class BookingRegisterComponent {
+    bookingForm: FormGroup;
+  
+    constructor(private formBuilder: FormBuilder) {
+      this.bookingForm = this.formBuilder.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        checkInDate: ['', Validators.required],
+        checkOutDate: ['', Validators.required]
+      });
+    }
+  
+    
