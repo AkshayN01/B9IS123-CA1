65b8ab8433a8e4555b050c8373200d9a21c6e29c
Jitty Thomas <jittythomas774@gmail.com>
2024-04-17 21:36:24 +0100

    booking-register

---
+++ src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/booking-register/booking-register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-booking-register',
+  templateUrl: './booking-register.component.html',
+  styleUrls: ['./booking-register.component.css']
+})
