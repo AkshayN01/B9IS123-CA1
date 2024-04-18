1dd5baf72bf8b9a2df764601cf97e0fab37df8cf
AkshayN01 <nambly77@gmail.com>
2024-04-18 17:57:11 +0100

    Allow angular app

--- src/HotelManagementSystem/Admin/HotelManagementSystem.Admin.API/Program.cs
+++ src/HotelManagementSystem/Admin/HotelManagementSystem.Admin.API/Program.cs
@@ -87,9 +87,8 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAnyOrigin",
-        builder => builder
-            .AllowAnyOrigin() // Allow requests from any origin
+    options.AddPolicy("AllowAngularApp",
+        builder => builder.WithOrigins("http://172.190.104.34")
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
@@ -117,7 +116,7 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
-app.UseCors("AllowAnyOrigin");
+app.UseCors("AllowAngularApp");
 app.UseAuthentication();
 
 app.UseAuthorization();
