e4e0ff1b6d1200fb69c15f212dae30c06c28997c
ChaithraUmesh21 <138147671+ChaithraUmesh21@users.noreply.github.com>
2024-04-18 23:10:48 +0100

    bugfix

--- src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/src/app/pages/protected/bookings/bookings.component.ts
+++ src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/src/app/pages/protected/bookings/bookings.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Booking } from '../../models/booking.model';
-import { BookingService } from '../../services/booking.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,19 +10,59 @@ import { Router } from '@angular/router';
 export class BookingsComponent implements OnInit {
   bookings: Booking[] = [];
 
-  constructor(private bookingService: BookingService, private router: Router) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.loadBookings();
-  }
-
-  loadBookings(): void {
-    this.bookingService.getAllBookings().subscribe(bookings => {
-      this.bookings = bookings;
-    });
+    this.bookings = [
+      {
+        id: 0,
+        fromDate: "0001-01-01T00:00:00",
+        toDate: "0001-01-01T00:00:00",
+        branchId: 0,
+        visitorDetails: {
+          visitorId: 1,
+          firstName: "John",
+          middleName: "A",
+          lastName: "Doe",
+          emailId: "john@example.com",
+          phoneNo: "1234567890",
+          isPrimary: 1
+        }
+      },
+      {
+        id: 2,
+        branchId: 1,
+        visitorDetails: {
+          visitorId: 2,
+          firstName: "Jane",
+          middleName: "A",
+          lastName: "Smith",
+          emailId: "jane@example.com",
+          phoneNo: "9876543210",
+          isPrimary: 1
+        },
+        fromDate: "2024-04-21",
+        toDate: "2024-04-25"
+      },
+      {
+        id: 3,
+        branchId: 2,
+        visitorDetails: {
+          visitorId: 3,
+          firstName: "Alice",
+          middleName: "A",
+          lastName: "Johnson",
+          emailId: "alice@example.com",
+          phoneNo: "5678901234",
+          isPrimary: 1
+        },
+        fromDate: "2024-04-26",
+        toDate: "2024-04-30"
+      }
+    ];
   }
 
   viewBooking(bookingId: number): void {
-        this.router.navigate(['/booking', bookingId]);
+    this.router.navigate(['/booking', bookingId]);
   }
 }
