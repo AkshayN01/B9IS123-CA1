b175893fd0edc303906d698c34b4049968bc3f9d
Jitty Thomas <jittythomas774@gmail.com>
2024-04-20 19:19:23 +0100

    Added role api

--- src/HotelManagementSystem/Admin/HotelManagementSystem.Admin.API/Controllers/RoleController.cs
+++ src/HotelManagementSystem/Admin/HotelManagementSystem.Admin.API/Controllers/RoleController.cs
@@ -1,6 +1,8 @@
-﻿using HotelManagementSystem.Contracts.Permissions;
+﻿using HotelManagementSystem.Contracts.APIModels.Admin;
+using HotelManagementSystem.Contracts.Permissions;
 using HotelManagementSystem.Library;
 using HotelManagementSystem.Library.Services;
+using HotelManagementSystem.Library.Services.Data.Admin;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,22 +10,44 @@ namespace HotelManagementSystem.Admin.API.Controllers
 {
     [ApiController]
     [Route("[controller]/api")]
-    [Authorize]
     public class RoleController : ControllerBase
     {
         private readonly UserService _userService;
         private readonly ILogger<RoleController> _logger;
         private readonly Blanket.Role.RoleBlanket _RoleBlanket;
 
-        public RoleController(ILogger<RoleController> logger, ManagementService managementService, UserService userService)
+        public RoleController(ILogger<RoleController> logger, ManagementService managementService, UserService userService, HotelBranchService hotelBranchService, IAdminUnitOfWork adminUnitOfWork)
         {
             _logger = logger;
             _userService = userService;
-            _RoleBlanket = new Blanket.Role.RoleBlanket(managementService, userService);
+            _RoleBlanket = new Blanket.Role.RoleBlanket(managementService, userService, hotelBranchService, adminUnitOfWork);
+        }
+
+        [HttpPost]
+        [Route("/api/userId/{userGuid}/addRole")]
+        public async Task<IActionResult> GetAllRoles(string userGuid, [FromBody]RoleModel roleModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            List<string> requiredPermission = new List<string>() { AdministratorPermissions.ViewRole };
+
+            bool hasPermission = await _userService.HasPermissions(userGuid, requiredPermission);
+            if (!hasPermission)
+                return Unauthorized();
+
+            try
+            {
+                var httpResponse = await _RoleBlanket.AddRole(userGuid, roleModel);
+                return Ok(httpResponse);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         [HttpGet]
-        [Route("/userId/{userGuid}/getAllRoles")]
+        [Route("/api/userId/{userGuid}/getAllRoles")]
         public async Task<IActionResult> GetAllRoles(string userGuid, int branchId)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
