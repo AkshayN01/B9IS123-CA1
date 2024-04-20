b88f2bb3e2390250949c59ba632b076a15da1186
Jitty Thomas <jittythomas774@gmail.com>
2024-04-20 18:39:16 +0100

    Added api

--- src/HotelManagementSystem/HotelManagementSystem.Contracts/Permissions/AdministratorPermissions.cs
+++ src/HotelManagementSystem/HotelManagementSystem.Contracts/Permissions/AdministratorPermissions.cs
@@ -12,5 +12,11 @@ namespace HotelManagementSystem.Contracts.Permissions
         public const string ViewRole = "admin:role:view";
         public const string EditRole = "admin:role:edit";
         public const string DeleteRole = "admin:role:delete";
+
+
+        public const string AddPermission = "admin:permission:add";
+        public const string ViewPermission = "admin:permission:view";
+        public const string EditPermission = "admin:permission:edit";
+        public const string DeletePermission = "admin:permission:delete";
     }
 }
