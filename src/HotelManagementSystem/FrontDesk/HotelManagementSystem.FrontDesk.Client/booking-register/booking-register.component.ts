c763885972ee1fbc09c7ffe881c3c9acced3917d
Jitty Thomas <jittythomas774@gmail.com>
2024-04-20 23:00:50 +0100

    adding more feature

--- src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/booking-register/booking-register.component.ts
+++ src/HotelManagementSystem/FrontDesk/HotelManagementSystem.FrontDesk.Client/booking-register/booking-register.component.ts
@@ -7,27 +7,44 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./booking-register.component.css']
 })
 export class BookingRegisterComponent {
-    bookingForm: FormGroup;
+  formData = {
+    firstname: '',
+    lastname:'',
+    email: '',
+    phone: '',
+    checkin: '',
+    checkout: '',
+    roomtype: ''
+  };
   
-    constructor(private formBuilder: FormBuilder) {
-      this.bookingForm = this.formBuilder.group({
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        checkInDate: ['', Validators.required],
-        checkOutDate: ['', Validators.required]
-      });
-    }
-    onSubmit() {
-        if (this.bookingForm.valid) {
-          // Process the booking data, e.g., send it to a backend server
-          console.log(this.bookingForm.value);
-          // Here you can make an HTTP request to your backend to save the booking
-        } else {
-          // Handle form validation errors
-          alert('Please fill out all required fields.');
-        }
-      }
-    }
-  
-    
+
+  persons: Person[] = []; // Array to store information about each person
+
+  addPerson() {
+    this.persons.push({ name: '', email: '', phone: '' });
+  }
+
+  removePerson(index: number) {
+    this.persons.splice(index, 1);
+  }
+
+  submitForm() {
+    // Handle form submission logic here
+    console.log('Form Data:', this.formData);
+    console.log('Persons:', this.persons);
+    this.clearFormData();
+  }
+
+  clearFormData() {
+    this.formData = {
+      firstname: '',
+      lastname:'',
+      email: '',
+      phone: '',
+      checkin: '',
+      checkout: '',
+      roomtype: ''
+    };
+    this.persons = [];
+  }
+}
