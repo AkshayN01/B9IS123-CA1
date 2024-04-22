42e4b0a147907271fd09982850002012693e2063
AkshayN01 <nambly77@gmail.com>
2024-04-22 21:46:51 +0100

    Fixed Entity Framework error

--- src/HotelManagementSystem/HotelManagementSystem.Library/Generic/Generic.cs
+++ src/HotelManagementSystem/HotelManagementSystem.Library/Generic/Generic.cs
@@ -16,5 +16,13 @@ namespace HotelManagementSystem.Library.Generic
 
             return Expression.Lambda<Func<T, bool>>(body, param);
         }
+
+        public static string GenerateRandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
     }
 }
